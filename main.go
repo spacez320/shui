255b546a8fe1387047724a9939790be447c58601
Matthew Coleman <matthew@eudaimonia.io>
2023-11-09 09:55:37 -0500

    Command evocation

--- main.go
+++ main.go
@@ -1,9 +1,44 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io"
+	"log"
+	"os/exec"
 )
 
+// General error manager.
+func e(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
 func main() {
-	fmt.Println("Hello, world.")
+	// Query to execute upon.
+	var query string
+
+	// Define arguments.
+	flag.StringVar(&query, "q", "whoami", "Query to execute.")
+	flag.Parse()
+
+	fmt.Printf("Executing query: '%s' ...\n", query)
+
+	// Prepare query execution.
+	cmd := exec.Command(query)
+	stdout, stdout_err := cmd.StdoutPipe()
+	e(stdout_err)
+
+	// Execute the query.
+	cmd_err := cmd.Start()
+	e(cmd_err)
+
+	// Interpret results.
+	cmd_output, cmd_output_err := io.ReadAll(stdout)
+	e(cmd_output_err)
+	fmt.Printf("Result is: \n%s\n", cmd_output)
+
+	// Clean-up.
+	cmd.Wait()
 }
