b1d027fdad38463ca68acc2bef34d6402cd3d985
Matthew Coleman <matthew@eudaimonia.io>
2023-12-25 19:49:06 -0500

    POC displaying a graph

--- internal/lib/results.go
+++ internal/lib/results.go
@@ -1,9 +1,16 @@
 //
 // Results management.
+//
+// Managing results involves:
+//
+// -  Organizing a storage of results.
+// -  Managing the TUI libraries, rendering, and interaction for results.
+// -  Finding a place for accessory output, like logs.
 
 package lib
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"strconv"
@@ -14,8 +21,18 @@ import (
 	"pkg/storage"
 
 	"github.com/gdamore/tcell/v2"
+	"github.com/mum4k/termdash"
+	"github.com/mum4k/termdash/cell"
+	"github.com/mum4k/termdash/container"
+	termdashTcell "github.com/mum4k/termdash/terminal/tcell"
+	"github.com/mum4k/termdash/terminal/terminalapi"
+	"github.com/mum4k/termdash/widgetapi"
+	"github.com/mum4k/termdash/widgets/sparkline"
 	"github.com/rivo/tview"
 )
+
+// Represents the display mode.
+type display_ int
 
 const (
 	LOGS_SIZE     = 1 // Proportional size of the logs widget.
@@ -23,28 +40,84 @@ const (
 	TABLE_PADDING = 2 // Padding for table cell entries.
 )
 
+const (
+	DISPLAY_RAW      display_ = iota + 1 // Used for direct output.
+	DISPLAY_TVIEW                        // Used when tview is the TUI driver.
+	DISPLAY_TERMDASH                     // Used when termdash is the TUI driver.
+
+)
+
 var (
-	app     *tview.Application // Application for display.
-	results storage.Results    // Stored results.
+	// Application for display. Only applicable for tview result modes.
+	app *tview.Application
+	// Display mode, dictated by the results.
+	mode display_
+	// Stored results.
+	results storage.Results
 
 	// Widget for displaying logs. Publicly offered to allow log configuration.
+	// Only applicable for tview result modes.
 	LogsView *tview.TextView
 )
 
+///////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Private
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
 // Set-up the sync for logs used by some result modes.
 func init() {
+	// Initialized specifically for showing logs in a tview pane. Currently,
+	// tview is the only supported display backend that supports logging, and
+	// termdash will not show logs.
+	//
+	// Initializing this is harmless, even if tview won't be used.
+	//
+	// TODO This should be probably be managed outside of init and should be made
+	// display mode agnostic, if possible.
 	LogsView = tview.NewTextView().SetChangedFunc(func() { app.Draw() })
 	LogsView.SetBorder(true).SetTitle("Logs")
 }
 
-// Sets-up the flex box, which defines the overall layout.
-func initDisplay(resultsView tview.Primitive, logsView tview.Primitive) {
+// Sets-up the termdash display and renders a widget.
+func initDisplayTermdash(resultsWidget widgetapi.Widget) {
+	// Set-up the context and enable it to close on key-press.
+	ctx, cancel := context.WithCancel(context.Background())
+
+	// Set-up the layout.
+	t, err := termdashTcell.New()
+	e(err)
+
+	// Render the widget.
+	c, err := container.New(t, container.PlaceWidget(resultsWidget))
+	e(err)
+
+	// Run the display.
+	termdash.Run(ctx, t, c, termdash.KeyboardSubscriber(
+		func(k *terminalapi.Keyboard) {
+			if k.Key == 'q' {
+				cancel()
+				t.Close()
+				os.Exit(0)
+			}
+		},
+	))
+}
+
+// Sets-up the tview flex box with results and logs views, which defines the
+// overall layout.
+//
+// Note that the app needs to be run separately from initialization in the
+// coroutine display function.
+func initDisplayTview(resultsView tview.Primitive, logsView tview.Primitive) {
+	// Initialize the app.
 	app = tview.NewApplication()
 
+	// Set-up the layout and apply views.
 	flexBox := tview.NewFlex().SetDirection(tview.FlexRow).
 		AddItem(resultsView, 0, RESULTS_SIZE, false).
 		AddItem(logsView, 0, LOGS_SIZE, false)
-
 	app.SetRoot(flexBox, true).SetFocus(resultsView)
 }
 
@@ -54,10 +127,22 @@ func display(f func()) {
 	// Execute the update function.
 	go func() { f() }()
 
-	// Start the display.
-	err := app.Run()
-	e(err)
+	switch mode {
+	case DISPLAY_TVIEW:
+		// Start the tview-specific display.
+		err := app.Run()
+		e(err)
+	case DISPLAY_TERMDASH:
+		// Start the termdash-specific display.
+		// Nothing to do, yet.
+	}
 }
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Public
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////
 
 // Adds a result to the result store.
 //
@@ -108,15 +193,34 @@ func TokenizeResult(result string) (parsedResult []interface{}) {
 	return
 }
 
+///////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Result Modes
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Presents raw output.
+func RawResults() {
+	mode = DISPLAY_RAW
+
+	go func() {
+		for {
+			fmt.Println(<-storage.PutEvents)
+		}
+	}()
+}
+
 // Update the results pane with new results as they are generated.
 func StreamResults() {
+	mode = DISPLAY_TVIEW
+
 	resultsView := tview.NewTextView().SetChangedFunc(
 		func() {
 			app.Draw()
 		})
 	resultsView.SetBorder(true).SetTitle("Results")
 
-	initDisplay(resultsView, LogsView)
+	initDisplayTview(resultsView, LogsView)
 
 	display(
 		func() {
@@ -127,21 +231,14 @@ func StreamResults() {
 	)
 }
 
-// Presents raw output.
-func RawResults() {
-	go func() {
-		for {
-			fmt.Println(<-storage.PutEvents)
-		}
-	}()
-}
-
 // Creates a table of results for the results pane.
 func TableResults() {
+	mode = DISPLAY_TVIEW
+
 	resultsView := tview.NewTable().SetBorders(true)
 	tableCellPadding := strings.Repeat(" ", TABLE_PADDING)
 
-	initDisplay(resultsView, LogsView)
+	initDisplayTview(resultsView, LogsView)
 
 	resultsView.SetDoneFunc(
 		func(key tcell.Key) {
@@ -180,6 +277,8 @@ func TableResults() {
 					row.SetCellSimple(i, j, tableCellPadding+nextCellContent+tableCellPadding)
 				}
 
+				// Re-draw the app with the new results row.
+				app.Draw()
 				i += 1
 			}
 		},
@@ -188,4 +287,29 @@ func TableResults() {
 	// Start the display.
 	err := app.Run()
 	e(err)
+}
+
+// Creates a graph of results for the results pane.
+func GraphResults() {
+	mode = DISPLAY_TERMDASH
+
+	graph, err := sparkline.New(
+		sparkline.Label("Test Sparkline", cell.FgColor(cell.ColorNumber(33))),
+		sparkline.Color(cell.ColorGreen),
+	)
+	e(err)
+
+	display(
+		func() {
+			for {
+				next := <-storage.PutEvents
+
+				graph.Add([]int{int(100 * next.Values[9].(float64))})
+			}
+
+			graph.Add([]int{1, 2, 3})
+		},
+	)
+
+	initDisplayTermdash(graph)
 }
